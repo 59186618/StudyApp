3d5241827dc0202226bec42afd9b0bbb088cbbe2
lijianyou-Herve <lijianyou.herve@gmail.com>
2016-10-25 23:35:37 +0800

    试题页面

--- app/src/main/java/com/example/herve/Study/ui/curriculum/adapter/CurriculumAdapter.java
+++ app/src/main/java/com/example/herve/Study/ui/curriculum/adapter/CurriculumAdapter.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.herve.Study.R;
 import com.example.herve.Study.bean.QuestionBean;
@@ -50,7 +51,36 @@ public class CurriculumAdapter extends RecyclerView.Adapter<CurriculumAdapter.Cu
     @Override
     public void onBindViewHolder(CurriculumViewHolder holder, int position) {
 
+        setTag(holder, position);
+
+        setListener(holder, holder.llSelectA);
+        setListener(holder, holder.llSelectB);
+        setListener(holder, holder.llSelectC);
+        setListener(holder, holder.llSelectD);
+
+
     }
+
+    private void setListener(final CurriculumViewHolder holder, View tvSelect) {
+        tvSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                holder.tvQuestion.append("( " + (String) view.getTag() + " )");
+            }
+        });
+
+
+    }
+
+    private void setTag(CurriculumViewHolder holder, int position) {
+        holder.llSelectA.setTag("A");
+        holder.llSelectB.setTag("B");
+        holder.llSelectC.setTag("C");
+        holder.llSelectD.setTag("D");
+
+
+    }
+
 
     @Override
     public int getItemCount() {
@@ -65,18 +95,26 @@ public class CurriculumAdapter extends RecyclerView.Adapter<CurriculumAdapter.Cu
         AppCompatCheckedTextView tvSelectTabA;
         @BindView(R.id.tv_select_a)
         AppCompatCheckedTextView tvSelectA;
+        @BindView(R.id.ll_select_a)
+        LinearLayout llSelectA;
         @BindView(R.id.tv_select_tab_b)
         AppCompatCheckedTextView tvSelectTabB;
         @BindView(R.id.tv_select_b)
         AppCompatCheckedTextView tvSelectB;
+        @BindView(R.id.ll_select_b)
+        LinearLayout llSelectB;
         @BindView(R.id.tv_select_tab_c)
         AppCompatCheckedTextView tvSelectTabC;
         @BindView(R.id.tv_select_c)
         AppCompatCheckedTextView tvSelectC;
+        @BindView(R.id.ll_select_c)
+        LinearLayout llSelectC;
         @BindView(R.id.tv_select_tab_d)
         AppCompatCheckedTextView tvSelectTabD;
         @BindView(R.id.tv_select_d)
         AppCompatCheckedTextView tvSelectD;
+        @BindView(R.id.ll_select_d)
+        LinearLayout llSelectD;
         @BindView(R.id.iv_solution)
         AppCompatImageView ivSolution;
         @BindView(R.id.tv_solution_exp)
